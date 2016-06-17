3781600fbe962173707e143568a8b21c0e1c9fd3
Abhishek Singh <abhishekkumarsingh.cse@gmail.com>
2016-06-17 17:13:15 +0530

    readContent and feed method added

--- feed.py
+++ feed.py
@@ -5,6 +5,7 @@ Takes extracted features from audio and feed it to the elasticsearch apis,
 which eventually creates and stores index of all extracted features.
 """
 
+import json
 from elasticsearch import Elasticsearch
 
 
@@ -20,3 +21,36 @@ class Feeder:
         uses localhost, port 9200 as default
         """
         self.es = Elasticsearch()
+
+    def readContent(self, jsonFile):
+        """
+        Converts json data to dictionary format
+
+        Input:
+            - jsonFile: a json file containing extracted features of an audio
+
+        output:
+            - content: features stored in dictionary format
+        """
+        with open(jsonFile, 'r') as jf:
+            content = json.load(jf)
+            return content
+
+    def feed(self, jsonFile, cont_id, iname="audio_index", doc_type="audio"):
+        """
+        feed extracted features of an audio to elasticsearch for index creation
+
+        Input:
+            - jsonFile: a json file of extracted features of an audio
+            - cont_id : identity of the content
+            - iname   : index name for that group of content
+            - doc_type: document type to be stored
+
+        output:
+            - retVal: a boolean return value. true if data is indexed otherwise
+                      false.
+        """
+        content = self.readContent(jsonFile)
+        ret = self.es.index(index=iname, doc_type, cont_id, body=content)
+        retVal = ret['created']
+        return retVal
